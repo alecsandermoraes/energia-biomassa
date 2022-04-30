04eadc4a48f937d93a2969afe2f322b65dd840cd
Alecsander Moraes <alecsandermoraesdossantos@gmail.com>
2022-04-29 22:34:02 -0300

    Script

--- script.py
+++ script.py
@@ -5,8 +5,8 @@ sg.theme('Dark')
 
 layout = [
     [sg.Text('Possuindo um valor de referência de algum outro local, insira a quantidade de energia produzida por certa quantidade de biomassa')],
-    [sg.Text('Quantidade de Biomassa (kg):'), sg.Input(key = 'biomassa')],
-    [sg.Text('Energia Liberada (kJ):'), sg.Input(key = 'energia')],
+    [sg.Text('Energia Liberada por um 1kg de Biomassa:'), sg.Input(key = 'energia')],
+    [sg.Text('Quantidade de Biomassa Desejada (kg):'), sg.Input(key = 'biomassa')],
     [sg.Button('Calcular')],
 ]
 
@@ -14,18 +14,21 @@ layout = [
 window = sg.Window('Cálculo de Energia a Biomassa', layout)
 
 while True:
-    events, values = window.read()
-    if events == sg.WIN_CLOSED:
-        window.close()
-    if values['energia'] == '' or values['biomassa'] == '':
-        sg.popup('Não foi possível calcular, pois os campos não foram preenchidos')
-        continue
     try:
-        result = (1 * int(values['energia'])) / int(values['biomassa'])
-        sg.popup('1kg de biomassa irá gerar: ' + str(result) + 'kJ de energia')
-    except ValueError:
-        sg.popup('Insira apenas números como valores')
-        continue
+        events, values = window.read()
+        if events == sg.WIN_CLOSED:
+            window.close()
+        if values['energia'] == '' or values['biomassa'] == '':
+            sg.popup('Não foi possível calcular, pois os campos não foram preenchidos')
+            continue
+        try:
+            result = int(values['energia']) * int(values['biomassa'])
+            sg.popup(f'{int(values["biomassa"])}kg de biomassa irá gerar: ' + str(result) + 'kJ de energia')
+        except ValueError:
+            sg.popup('Insira apenas números como valores')
+            continue
+        except TypeError:
+            break
     except TypeError:
         break
 
